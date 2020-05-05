f19ee613493a2e04353018587a02153508a2165e
Justin James <justin.james@yale.edu>
2020-05-05 12:48:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  {"3" => "SixNine"
+  {"3" => "SixNine"}
 end
 
 def pioneer
-  {:name => "# return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  {:id => number }
 end
